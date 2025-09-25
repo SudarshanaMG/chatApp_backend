2135caab88b501ef736f984bda6c76f74a1bc51a
SudarshanaMG <sudarshanamgpv48@gmail.com>
2025-09-26 02:22:34 +0530

    commented

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import { auth, db } from './firebase.js';
-import { v2 as cloudinary } from 'cloudinary';
-import multer from 'multer';
+// import { v2 as cloudinary } from 'cloudinary';
+// import multer from 'multer';
 
 const app = express();
 const port = 3000;
@@ -10,46 +10,46 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-cloudinary.config({
-  cloud_name: 'cloudName',
-  api_key: 'APIKEY',
-  api_secret: 'apiSecret',
-});
+// cloudinary.config({
+//   cloud_name: 'cloudName',
+//   api_key: 'APIKEY',
+//   api_secret: 'apiSecret',
+// });
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+// const storage = multer.memoryStorage();
+// const upload = multer({ storage });
 
-const uploadImageToCloudinary = (fileBuffer) => {
-  return new Promise((resolve, reject) => {
-    const stream = cloudinary.uploader.upload_stream(
-      {
-        resource_type: 'image',
-        use_filename: true,
-        unique_filename: false,
-        overwrite: true,
-      },
-      (error, result) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(result.secure_url);
-        }
-      }
-    );
+// const uploadImageToCloudinary = (fileBuffer) => {
+//   return new Promise((resolve, reject) => {
+//     const stream = cloudinary.uploader.upload_stream(
+//       {
+//         resource_type: 'image',
+//         use_filename: true,
+//         unique_filename: false,
+//         overwrite: true,
+//       },
+//       (error, result) => {
+//         if (error) {
+//           reject(error);
+//         } else {
+//           resolve(result.secure_url);
+//         }
+//       }
+//     );
 
-    stream.end(fileBuffer);
-  });
-};
+//     stream.end(fileBuffer);
+//   });
+// };
 
 app.post('/register', upload.single('file'), async (req, res) => {
-  const { email, password, userName } = req.body;
-  const file = req.file;
-  let imageUri = '';
+  const { email, password, userName, imageUri } = req.body;
+  // const file = req.file;
+  // let imageUri = '';
 
   try {
-    if (file) {
-      imageUri = await uploadImageToCloudinary(file.buffer);
-    }
+    // if (file) {
+    //   imageUri = await uploadImageToCloudinary(file.buffer);
+    // }
     const user = await auth.createUser({
       email,
       password,
@@ -65,7 +65,7 @@ app.post('/register', upload.single('file'), async (req, res) => {
       createdAt: Date.now(),
     });
 
-    res.status(201).json({ message: 'User registered successfully', uid: user.uid });
+    res.status(201).json({ message: 'User registered successfully', uid: user.uid, imageUri });
   } catch (error) {
     console.error('Error in /register:', error.message);
     res.status(400).json({ message: error.message });
